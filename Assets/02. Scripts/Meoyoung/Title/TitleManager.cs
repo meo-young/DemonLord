c5e40328d05b6fafcfcce504506a667d9120aeea
meo-young <meoyoung000@naver.com>
2025-03-01 16:39:06 +0900

    [Add] TitleManager 구현

---
+++ Assets/02. Scripts/Meoyoung/Title/TitleManager.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleManager : MonoBehaviour
+{
+    [Header("시작 클릭 시 넘어갈 씬 이름")]
+    [SerializeField] private string startSceneName;
+
+    public void OnStartButtonClick()
+    {
+        // 페이드 아웃 후 시작 씬으로 이동
+        FadeManager.instance.FadeOut(onComplete :
+            () => {
+                SceneManager.LoadScene(startSceneName);
+            }
+        );
+    }
+
+    public void OnExitButtonClick()
+    {
+        // 페이드 아웃 후 게임 종료
+        FadeManager.instance.FadeOut(onComplete :
+            () => {
+                #if UNITY_EDITOR
+                    UnityEditor.EditorApplication.isPlaying = false;
+                #else
+                    Application.Quit();
+                #endif
+            }
+        );
+    }
+
+    public void OnCreditButtonClick()
+    {
+        // @To-do 크레딧 화면 표시
+        Debug.Log("크레딧 화면 표시");
+    }
+}
