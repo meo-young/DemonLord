e8bf139eb8ab2215f7ebf6c461ee4bc5744677eb
meo-young <meoyoung000@naver.com>
2025-03-02 03:44:09 +0900

    [Update] 플레이어 사망 로직 추가

---
+++ Assets/02. Scripts/Meoyoung/Character/Player.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Player : CharacterBase
+{
+    // @TODO : 권능, 특성 추가
+
+    protected override void Start()
+    {
+        base.Start();
+
+        Debug.Log($"{characterName} 캐릭터 생성");
+
+        // 파티에 플레이어 추가
+        PartyManager.instance.AddMember(this);
+
+        // Test
+        //SelectionUI.instance.InitSelectionUI();
+    }
+
+    protected override void OnDeath()
+    {
+        base.OnDeath();
+
+        Debug.Log("게임 종료 해라");
+        
+    }
+}
