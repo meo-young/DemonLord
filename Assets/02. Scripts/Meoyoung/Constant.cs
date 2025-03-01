2d8be5cc9a52aaa0b75e898b04e3464943b838c1
meo-young <meoyoung000@naver.com>
2025-03-01 20:41:26 +0900

    [Add] 상수 클래스 추가

---
+++ Assets/02. Scripts/Meoyoung/Constant.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public static class Constant
+{
+    [Header("권능 관련 수치")]
+    public const int HEAL_WARRANT_HEAL_AMOUNT = 10;
+    public const int RANDOM_WARRANT_DAMAGE_AMOUNT = 10;
+}
