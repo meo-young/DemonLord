5397e12f3aaa9f6b5835527cc47056667fa1e62e
meo-young <meoyoung000@naver.com>
2025-03-02 21:07:16 +0900

    [Add] 함정 UI 구현

---
+++ Assets/02. Scripts/Meoyoung/Trap/TrapUI.cs
@@ -0,0 +1,111 @@
+using UnityEngine;
+
+public class TrapUI : MonoBehaviour
+{
+    public static TrapUI instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+
+
+    /// <summary>
+    /// 함정 UI 활성화
+    /// </summary>
+    public void ShowTrapUI()
+    {
+        // 주사위 결과 텍스트 초기화
+        Dice.instance.InitDiceBtn();
+
+        // UI 활성화
+        transform.localScale = Vector3.one;
+        
+        // 상황 텍스트 수정
+        SituationUI.instance.SetSituationText("도적의 가호는 데미지 증가와 함정에 유리해집니다.");
+    }
+
+
+
+    /// <summary>
+    /// 도적의 가호 버튼 클릭 시 이벤트 추가
+    /// </summary>
+    public void OnClickTrapUI()
+    {
+        // 주사위 이벤트 추가
+        Dice.instance.AddDiceResultEvent(CheckTrapEvent);
+    }
+
+
+
+
+    /// <summary>
+    /// 함정 UI 숨김
+    /// </summary>
+    private void HideTrapUI()
+    {
+        transform.localScale = Vector3.zero;
+    }
+
+    
+
+    /// <summary>
+    /// 주사위 굴린 후 어떤 이벤트인지 텍스트 출력
+    /// </summary>
+    private void CheckTrapEvent()
+    {
+        int diceValue = GameManager.instance.currentDiceResultInt;
+
+        int trapHandicap = GameManager.instance.trapHandicap;
+
+        if(GameManager.instance.isThiefItem)
+        {
+            if (diceValue <= 3 - trapHandicap)
+            {
+                AttackAllPartyMembers();
+                SituationUI.instance.SetSituationText("파티가 함정에 빠지게 되었습니다");
+            }
+            else
+            {
+                SituationUI.instance.SetSituationText("무사히 탈출에 성공하였습니다");
+            }
+        }
+        else
+        {
+            if (diceValue <= 6 - trapHandicap)
+            {
+                AttackAllPartyMembers();
+                SituationUI.instance.SetSituationText("파티가 함정에 빠지게 되었습니다");
+            }
+            else
+            {
+                SituationUI.instance.SetSituationText("무사히 탈출에 성공하였습니다");
+            }
+        }
+        
+
+        Invoke("TempFadeOut", 1.5f);
+    }
+
+    private void AttackAllPartyMembers()
+    {
+        foreach (var member in PartyManager.instance.GetPartyMembers())
+        {
+            member.GetDamage(5);
+        }
+    }
+
+
+
+    private void TempFadeOut()
+    {
+        FadeManager.instance.FadeOut(
+            () =>
+            {
+                HideTrapUI();
+                BattleManager.instance.StartBattle();
+            }
+        );
+    }
+}
