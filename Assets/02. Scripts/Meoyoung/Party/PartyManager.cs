21a4c7fb2cd75f1574f916f7fce8499f7344261b
meo-young <meoyoung000@naver.com>
2025-03-02 15:23:53 +0900

    [Update] PartyManager 유틸 함수 추가

--- Assets/02. Scripts/Meoyoung/Party/PartyManager.cs
+++ Assets/02. Scripts/Meoyoung/Party/PartyManager.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEngine.UI;
 
 public class PartyManager : MonoBehaviour
 {
@@ -21,41 +22,137 @@ public class PartyManager : MonoBehaviour
         }
     }
 
-    // 현재 파티에 캐릭터 추가
+
+
+    /// <summary>
+    /// 현재 파티에 캐릭터 추가
+    /// </summary>
+    /// <param name="member">추가할 캐릭터</param>
     public void AddMember(CharacterBase member)
     {
         partyMembers.Add(member);
         PartyInfoUI.instance.AddPartyInfo(member, partyMembers.Count - 1);
     }
 
-    // 파티 멤버 반환
+
+
+    /// <summary>
+    /// 현재 파티 멤버 반환
+    /// </summary>
+    /// <returns>현재 파티 멤버</returns>
     public List<CharacterBase> GetPartyMembers()
     {
         return partyMembers;
     }
 
-    // 특정 직업 캐릭터 반환
+
+
+    /// <summary>
+    /// 특정 직업 캐릭터 반환
+    /// </summary>
+    /// <param name="type">찾을 캐릭터 타입</param>
+    /// <returns>특정 직업 캐릭터 반환</returns>
     public CharacterBase GetCharacterByType(CharacterType type)
     {
         return partyMembers.Find(member => member.characterType == type);
     }
 
-    // 파티 멤버 수 반환
+
+
+    /// <summary>
+    /// 파티 멤버 수 반환
+    /// </summary>
+    /// <returns>파티 멤버 수 반환</returns>
     public int GetPartyMemberNum()
     {
         return partyMembers.Count;
     }
 
-    // 생존 중인 파티원 중 체력 상태가 가장 나쁜 캐릭터 반환
+
+
+    /// <summary>
+    /// 생존 중인 파티원 중 체력 상태가 가장 나쁜 캐릭터 반환
+    /// </summary>
+    /// <returns>생존 중인 파티원 중 체력 상태가 가장 나쁜 캐릭터 반환</returns>
     public CharacterBase GetWorstHealthCharacter()
     {
         return partyMembers.Where(member => member.isAlive).OrderBy(member => member.currentHealth).First();
     }
 
-    // 동일한 직업군이 있는지 반환
+
+
+    /// <summary>
+    /// 동일한 직업군이 있는지 반환
+    /// </summary>
+    /// <param name="characterType">찾을 캐릭터 타입</param>
+    /// <returns>동일한 직업군 여부 반환</returns>
     public bool IsSameCharacterTypeInParty(CharacterType characterType)
     {
         return partyMembers.Any(member => member.characterType == characterType);
     }
-    
+
+
+
+    /// <summary>
+    /// 생존해 있는 파티 멤버들만 반환
+    /// </summary>
+    /// <returns>생존해 있는 파티 멤버들만 반환</returns>
+    public List<CharacterBase> GetAlivePartyMembers()
+    {
+        return partyMembers.Where(member => member.isAlive).ToList();
+    }
+
+
+
+    /// <summary>
+    /// 반대 상성 관계에 있는 캐릭터 반환
+    /// </summary>
+    /// <param name="type">찾을 캐릭터 타입</param>
+    /// <returns>반대 상성 관계에 있는 캐릭터 반환</returns>
+    public CharacterBase GetReverseCounterTypeCharacter(CharacterType type)
+    {
+        CharacterType counterType = type switch
+        {
+            CharacterType.Warrior => CharacterType.Ranger,
+            CharacterType.Ranger => CharacterType.Wizard,
+            CharacterType.Wizard => CharacterType.Warrior,
+            _ => throw new System.ArgumentException("지원하지 않는 캐릭터 타입입니다.")
+        };
+
+        CharacterBase character = GetCharacterByType(counterType);
+        return character != null && character.isAlive ? character : null;
+    }
+
+
+
+    /// <summary>
+    /// 같은 타입의 캐릭터 반환
+    /// </summary>
+    /// <param name="type">찾을 캐릭터 타입</param>
+    /// <returns>같은 타입의 캐릭터 반환</returns>
+    public CharacterBase GetSameTypeCharacter(CharacterType type)
+    {
+        return partyMembers.Find(member => member.characterType == type && member.isAlive);
+    }
+
+
+
+    /// <summary>
+    /// 상성 관계에 있는 캐릭터 반환
+    /// </summary>
+    /// <param name="type">찾을 캐릭터 타입입</param>
+    /// <returns>상성 관계에 있는 캐릭터 반환</returns>
+    public CharacterBase GetCounterTypeCharacter(CharacterType type)
+    {
+       CharacterType counterType = type switch
+        {
+            CharacterType.Warrior => CharacterType.Wizard,
+            CharacterType.Ranger => CharacterType.Warrior,
+            CharacterType.Wizard => CharacterType.Ranger,
+            _ => throw new System.ArgumentException("지원하지 않는 캐릭터 타입입니다.")
+        };
+
+        CharacterBase character = GetCharacterByType(counterType);
+        return character != null && character.isAlive ? character : null;
+    }
 }
