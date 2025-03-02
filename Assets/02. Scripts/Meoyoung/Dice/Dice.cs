ef2cd5b385ce104b063093b9bccc4ee2f6e724fb
meo-young <meoyoung000@naver.com>
2025-03-02 19:10:34 +0900

    [Update] Dice 로직 추가 구현

--- Assets/02. Scripts/Meoyoung/Dice/Dice.cs
+++ Assets/02. Scripts/Meoyoung/Dice/Dice.cs
@@ -1,4 +1,8 @@
 using UnityEngine;
+using System.Collections;
+using System;
+using TMPro;
+using UnityEngine.UI;
 
 /* 주사위의 결과는 다음과 같은 타입으로 나온다.
 * 2~4 : Bad
@@ -16,6 +20,19 @@ public class Dice : MonoBehaviour
 {
     public static Dice instance;
 
+
+    [Header("주사위 결과 텍스트")]
+    public TMP_Text diceResultText;
+
+    [Header("주사위 버튼")]
+    public Button diceButton;
+
+    [Header("주사위 굴리는 애니메이션")]
+    public Animator diceAnimator;
+
+    private int diceResult;
+    private Action diceResultEvent;
+
     private void Awake()
     {
         if(instance == null)
@@ -28,17 +45,24 @@ public class Dice : MonoBehaviour
         }
     }
 
+
+
+    /// <summary>
+    /// 주사위를 굴리고 DiceType으로로 결과를 반환
+    /// </summary>
+    /// <returns>주사위 결과</returns>
     public DiceType Roll()
     {
         // 랜덤으로 주사위를 굴림
-        int result = Random.Range(2, 13);
+        diceResult = UnityEngine.Random.Range(2, 13);
+        GameManager.instance.currentDiceResultInt = diceResult;
 
         // 결과에 따라 주사위 타입을 반환
-        if(result >= 2 && result <= 4)
+        if(diceResult >= 2 && diceResult <= 4)
         {
             return DiceType.Bad;
         }
-        else if(result >= 5 && result <= 10)
+        else if(diceResult >= 5 && diceResult <= 10)
         {
             return DiceType.Normal;
         }
@@ -46,5 +70,76 @@ public class Dice : MonoBehaviour
         {
             return DiceType.Good;
         }
+    }
+
+
+    /// <summary>
+    /// 주사위 굴린 후 등록된 이벤트 호출
+    /// </summary>
+    /// <param name="diceResultEvent">등록할 이벤트</param>
+    public void AddDiceResultEvent(Action diceResultEvent)
+    {
+        // 이미 이벤트가 있으면 제거
+        if(this.diceResultEvent != null)
+        {
+            this.diceResultEvent = null;
+        }
+
+        this.diceResultEvent += diceResultEvent;
+    }
+
+
+
+    /// <summary>
+    /// 주사위 결과 텍스트 초기화
+    /// </summary>
+    public void InitDiceResultText()
+    {
+        diceResultText.rectTransform.localScale = Vector3.zero;
+        diceResultText.text = "";
+    }
+
+
+
+    /// <summary>
+    /// 주사위를 굴리는 애니메이션 실행
+    /// </summary>
+    public void RollWithAnimation()
+    {
+        // 주사위 버튼 비활성화
+        diceButton.enabled = false;
+
+        Roll();
+        StartCoroutine(RollDice());
+    }
+
+
+
+    /// <summary>
+    /// 주사위 굴리는 애니메이션 실행
+    /// 1.5초 후 주사위 결과 텍스트 표시
+    /// 0.5초 후 등록된 이벤트 호출
+    /// </summary>
+    private IEnumerator RollDice()
+    {
+        // 주사위 굴리는 애니메이션 실행
+        diceAnimator.SetBool("Roll", true);
+
+        yield return new WaitForSeconds(1.5f);
+
+        // 주사위 결과 텍스트 표시
+        diceResultText.rectTransform.localScale = Vector3.one;
+        diceResultText.text = diceResult.ToString();
+
+        // 주사위 굴리는 애니메이션 종료
+        diceAnimator.SetBool("Roll", false);
+
+        yield return new WaitForSeconds(0.5f);
+
+        // 등록된 이벤트 호출
+        diceResultEvent?.Invoke();
+
+        // 주사위 버튼 활성화
+        diceButton.enabled = true;
     }
 }
